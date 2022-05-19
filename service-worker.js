d4fdb9c5f6cbf93577d51e50b89a496154204b37
MikeSchsr13 <micheleschiesaro13@gmail.com>
2022-05-19 22:29:12 +0200

    pwa

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,28 @@
+// Evento install
+self.addEventListener('install', (event) => {
+	// Codice da eseguire su installazione
+	console.log('Service Worker Installato');
+});
+// Evento activate
+self.addEventListener('activate', (event) => {
+	// Codice da eseguire su attivazione
+	console.log('Service Worker Attivo');
+});
+// Evento fetch
+// self.addEventListener('fetch', (event) => {
+// 	// Codice da eseguire su fetch di risorse
+// 	console.log('Richiesta URL: ' + event.request.url);
+// });
 
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(fetch(event.request));
+// });
 
-self.addEventListener('install', function() {
-    console.log('Install!');
-  });
-  self.addEventListener("activate", event => {
-    console.log('Activate!');
-  });
-  self.addEventListener('fetch', function(event) {
-    console.log('Fetch!', event.request);
-  });
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request).then(function(response) {
+            cache.put(event.request, response.clone());
+            return response;
+        })
+    );
+});
